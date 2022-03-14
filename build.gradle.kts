a432b0f673a82db481cb1091bdfba41f987e8625
mariohuq <mariohuq@github.com>
2022-03-14 19:16:35 +0300

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     java
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_16
+}
+
 group = "ru.mail.polis"
 version = "1.0-SNAPSHOT"
 
@@ -10,7 +15,23 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    // Annotation
+    implementation("org.jetbrains:annotations:22.0.0") {
+        because("we need @NotNull, etc.")
+    }
+
+    testImplementation("org.seleniumhq.selenium:selenium-java:4.1.2") {
+        because("we want to use Selenium to remote control browsers")
+    }
+
+    // https://selenide.org/quick-start.html
+    testImplementation("com.codeborne:selenide:6.3.3") {
+        because("we want to use Selenide too, to remote control browsers")
+    }
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2") {
+        because("we want to use JUnit 5 assertions")
+    }
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
