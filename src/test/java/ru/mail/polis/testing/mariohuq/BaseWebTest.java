253087431715c4eff3727948356b56b26bb02dfd
mariohuq <mariohuq@github.com>
2022-03-22 08:11:34 +0300

    Optimize imports

--- src/test/java/ru/mail/polis/testing/mariohuq/BaseWebTest.java
+++ src/test/java/ru/mail/polis/testing/mariohuq/BaseWebTest.java
@@ -1,15 +1,12 @@
 package ru.mail.polis.testing.mariohuq;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import ru.mail.polis.testing.mariohuq.pages.LoginPage;
-import ru.mail.polis.testing.mariohuq.pages.MainPage;
 import ru.mail.polis.testing.mariohuq.utils.User;
 
-import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Configuration.*;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 /**
  * Нужно добавить переменные окружения. Пример:
