6a40dd7b0dc91313a3df7d67e5eacd341556fa98
AirBurn <rosan1233456@gmail.com>
2022-04-10 16:29:55 +0300

    fix

--- src/main/java/ru/mail/polis/testing/mariohuq/pages/MainPage.java
+++ src/main/java/ru/mail/polis/testing/mariohuq/pages/MainPage.java
@@ -8,7 +8,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class MainPage implements CheckablePage<MainPage> {
-    private final SelenideElement profileLink = $(By.xpath("//a[@data-l='t,userPage']"));
+    private final SelenideElement profileLink = $(By.xpath("//*[@id='hook_Block_Navigation']//a[@data-l='t,userPage']"));
     private final SelenideElement recommendationsLink = $(By.xpath("//a[@href='/dk?cmd=MainFeedsWrapper&st.f.block=MainFeedsContent&st.f.filterGroupId=43&st.cmd=userMain&st._aid=Feed_Filter']"));
 
     public final ElementsCollection feedPostsHideLinks = $$(By.xpath("//*[@class='feed-w']//a[@class='feed_close']"));
