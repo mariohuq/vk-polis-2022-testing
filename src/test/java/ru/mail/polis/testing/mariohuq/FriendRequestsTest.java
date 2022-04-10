edab325d2c018c596cb24cd364adc85a0c8bb53f
AirBurn <rosan1233456@gmail.com>
2022-04-11 00:32:58 +0300

    fix

--- src/test/java/ru/mail/polis/testing/mariohuq/FriendRequestsTest.java
+++ src/test/java/ru/mail/polis/testing/mariohuq/FriendRequestsTest.java
@@ -24,7 +24,6 @@ public class FriendRequestsTest extends BaseWebTest {
     }
 
     @Test
-
     public void removeFriend() {
         assertThat(is(new FriendsPage(SENT_REQUEST)
                 .hasFriendRequest(TARGET_USER_NAME)
